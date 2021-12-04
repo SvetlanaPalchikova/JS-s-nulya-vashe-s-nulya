76269dda8bf7e48191f96550077572be333292d3
Svetlana Krasnonosenkikh <svetik.palchik81@gmail.com>
2021-12-04 19:30:50 +0300

    iesson 11

--- TSX/index.tsx
+++ TSX/index.tsx
@@ -13,16 +13,23 @@ let avatarWrapperEl = document.getElementById(avatarWrapperId);
 let avatarId = "avatar";
 let avatarEl = document.getElementById(avatarId);
 
+function alertValue() {
+    console.log(firstNameEl.value);
+    console.log(lastNameEl.value);
+    console.log(addressEl.value);
+    console.log(citiesEl.value);
+}
 firstNameEl.value = "Andrey";
-//lastNameEl.setAttribute("value", "kamasutra";
-
-//  window.alert(lastNameEl.className);
+alertValue();
 lastNameEl.className = "last-name default-input error-input";
+
+alertValue();
+
 avatarEl.src = "https://vk.com/photo502314705_456239018?all=1";
 avatarEl.title = "i am avatar";
-hobbiesEl.value = "I am not interest";
+hobbiesEl.Value = "I am not interest";
 avatarWrapperEl.innerHTML = "<ul id='list'></ul> <li>1</li> <li>2 </li>";
 let listId = 'list';
-let numberlist = document.getElementById(listId);
+let numberList = document.getElementById(listId);
 
-window.alert(numberlist.innerHTML);
+window.alert(numberList.innerHTML);
